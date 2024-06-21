9e054ee172793359f18d5b1ec747408bd07cd303
dillabadu2138 <dillabadu2138@gmail.com>
2024-06-22 03:18:32 +0900

    add lip sync

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,6 +1,20 @@
 import React, { useState, useRef, useEffect, useMemo } from "react";
+import { useLoader, useFrame } from "@react-three/fiber";
 import { useGLTF, useFBX, useAnimations } from "@react-three/drei";
 import { useControls } from "leva";
+import * as THREE from "three";
+
+const lookUpTable = {
+  A: "viseme_PP",
+  B: "viseme_kk",
+  C: "viseme_E",
+  D: "viseme_aa",
+  E: "viseme_O",
+  F: "viseme_U",
+  G: "viseme_FF",
+  H: "viseme_TH",
+  X: "viseme_sil",
+};
 
 export function Avatar(props) {
   const { playAudio, script } = useControls({
@@ -12,6 +26,42 @@ export function Avatar(props) {
   });
 
   const audio = useMemo(() => new Audio(`/audios/${script}.mp3`), [script]);
+  const jsonFile = useLoader(THREE.FileLoader, `audios/${script}.json`);
+  const lipsync = JSON.parse(jsonFile);
+
+  useFrame(() => {
+    const currentAudioTime = audio.currentTime;
+
+    if (audio.paused || audio.ended) setAnimation("Idle");
+
+    // reset morphTargetInfluences
+    Object.values(lookUpTable).forEach((value) => {
+      nodes.Wolf3D_Head.morphTargetInfluences[
+        nodes.Wolf3D_Head.morphTargetDictionary[value]
+      ] = 0;
+      nodes.Wolf3D_Teeth.morphTargetInfluences[
+        nodes.Wolf3D_Teeth.morphTargetDictionary[value]
+      ] = 0;
+    });
+
+    for (let i = 0; i < lipsync.mouthCues.length; i++) {
+      const mouthCue = lipsync.mouthCues[i];
+
+      if (
+        currentAudioTime >= mouthCue.start &&
+        currentAudioTime <= mouthCue.end
+      ) {
+        nodes.Wolf3D_Head.morphTargetInfluences[
+          nodes.Wolf3D_Head.morphTargetDictionary[lookUpTable[mouthCue.value]]
+        ] = 1;
+        nodes.Wolf3D_Teeth.morphTargetInfluences[
+          nodes.Wolf3D_Teeth.morphTargetDictionary[lookUpTable[mouthCue.value]]
+        ] = 1;
+
+        break;
+      }
+    }
+  });
 
   useEffect(() => {
     if (playAudio) {
